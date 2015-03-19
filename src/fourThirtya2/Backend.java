9beaa3d28d7e60a9569b5da8b1403a90e384fef1
Jim <Jim@129.82.98.57>
2015-03-19 16:09:31 -0600

    Final commit

--- src/fourThirtya2/Backend.java
+++ src/fourThirtya2/Backend.java
@@ -2,13 +2,30 @@ package fourThirtya2;
 
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Backend {
 
 	private static Connection conn;
+	
+	/*
+	 * This is my attempt to always close the connection on exit. This
+	 * uses a shutdown hook to accomplish 
+	 */
+	static class OnClose extends Thread {
+		public void run(){
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 	/*
 	 * The main call for the application. Creates the SSH tunnel, the JDBC
@@ -18,6 +35,7 @@ public class Backend {
 		TunneledConnection tunnel = new TunneledConnection();
 		conn = tunnel.createTunnel();
 		if (conn != null) {
+			Runtime.getRuntime().addShutdownHook(new OnClose() );
 			System.out.println("--> Connection Open");
 			ModelView view = new ModelView();
 			view.run();
@@ -42,11 +60,229 @@ public class Backend {
 		Statement stmt;
 		ResultSet rs = null;
 		try {
-			stmt = conn.createStatement();
+			stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
 			rs = stmt.executeQuery("SELECT * FROM " + name);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return rs;
+	}
+	
+	//TELLS ME IF I HAVE THE BOOK ALREADY
+	public static boolean haveBook(int ISBN){
+		PreparedStatement stmt;
+		ResultSet rs;
+		try {
+			stmt = conn.prepareStatement("SELECT * FROM books WHERE isbn_num=?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			stmt.setInt(1, ISBN);
+			rs = stmt.executeQuery();
+			int size = -1;
+			try {
+				rs.beforeFirst();
+				rs.last();
+				size = rs.getRow();
+				rs.beforeFirst();
+			} catch (SQLException e) {
+				System.out.println(e);
+			}
+			//MEANS DATABASE DOESN'T HAVE THE BOOK
+			if (size >0){
+				return true;
+			}
+			else {
+				return false;
+			}
+		} catch (Exception e){
+			System.out.println(e);
+			return false;
+		}
+		
+	}
+
+	public static void addStudent(int iD, String fname, String lname, String degree) {
+		PreparedStatement stmt;
+		try {
+			stmt = conn.prepareStatement("INSERT INTO students (studentid, first_name, last_name, degree) VALUES" + "(?, ?, ?, ?)", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			stmt.setInt(1, iD);
+			stmt.setString(2, fname);
+			stmt.setString(3, lname);
+			stmt.setString(4, degree);
+			stmt.executeUpdate();
+			System.out.println("Table insert successful.");
+		} catch (Exception e){
+			System.out.println(e);
+		}
+		
+	}
+
+	public static boolean removeStudent(int iD) {
+		//check if student had any books.
+		PreparedStatement stmt;
+		ResultSet rs;
+		try {
+			//returns a list of bookIDs associated with the student
+			stmt = conn.prepareStatement("SELECT bookissued FROM books2students WHERE studentid = ?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			stmt.setInt(1, iD);
+			int size = -1;
+			rs = stmt.executeQuery();
+			if (rs != null) {
+				rs.beforeFirst();
+				rs.last();
+				size = rs.getRow();
+				rs.beforeFirst();
+				int[] books = new int[size];
+				int i = 0;
+				while (rs.next()) {
+					int temp = rs.getInt(1);
+					books[i] = temp;
+					i++;
+				}
+				//now have all the bookIDs in an array, just need to remove all associations. Delete relation from students2books and incriment on books.
+				stmt = conn.prepareStatement("DELETE FROM books2students WHERE studentid=?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+				for (int j = 0; j < books.length; j++){
+					incrimentNumCount(books[j]);
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		try {
+			stmt = conn.prepareStatement("DELETE FROM students WHERE studentid=?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			stmt.setInt(1, iD);
+			stmt.executeUpdate();
+			System.out.println("Table deletion successful.");
+			return true;
+		} catch (Exception e){
+			System.out.println(e);
+			return false;
+		}
+	}
+
+	public static void incrimentNumCount(int ISBN) {
+		PreparedStatement stmt;
+		try {
+			stmt = conn.prepareStatement("UPDATE books SET numcopiesavail = numcopiesavail + 1 WHERE isbn_num=?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			stmt.setInt(1, ISBN);
+			stmt.executeUpdate();
+		} catch (Exception e){
+			System.out.println(e);
+		}
+		System.out.println("Bookcount has been incrimented.");	
+	}
+
+	public static void addBook(int ISBN, String name, int year) {
+		PreparedStatement stmt;
+		try {
+			stmt = conn.prepareStatement("INSERT INTO books (isbn_num, name, yearofpublication, numcopiesavail) VALUES" + "(?, ?, ?, ?)", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			stmt.setInt(1, ISBN);
+			stmt.setString(2, name);
+			stmt.setInt(3, year);
+			stmt.setInt(4, 1);
+			stmt.executeUpdate();
+			System.out.println(name + " has been added to the database.");
+		} catch (Exception e){
+			System.out.println("Error adding the book.");
+		}
+	}
+
+	public static void decrimentBookCount(int iD) {
+		PreparedStatement stmt;
+		ResultSet rs;
+		try {
+			stmt = conn.prepareStatement("SELECT numcopiesavail FROM books WHERE isbn_num=?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			stmt.setInt(1, iD);
+			rs = stmt.executeQuery();
+			if (rs.next()) {
+				int numHave = rs.getInt(1);
+				if (numHave > 0) {
+					try {
+						stmt = conn.prepareStatement("UPDATE books SET numcopiesavail = numcopiesavail -1 WHERE isbn_num=?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+						stmt.setInt(1, iD);
+						stmt.executeUpdate();
+						System.out.println("Lowered count of books for " + iD);
+					} catch (Exception e) {
+						System.out.println(e + "204");
+					}
+				} else {
+					// have to remove the book
+					System.out.println("No aditional copies of " + iD + " available");
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(e + "212");
+		}
+	}
+
+	static void removeBook(int iD) {
+		PreparedStatement stmt;
+		try {
+			stmt = conn.prepareStatement("DELETE FROM books WHERE isbn_num=?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			stmt.setInt(1, iD);
+			stmt.execute();
+			System.out.println("Book ID: " + iD + " has been removed from the system.");
+		} catch (Exception e){
+			System.out.println(e + "224");
+		}
+	}
+
+	public static boolean Available(int iSBN) {
+		PreparedStatement stmt;
+		ResultSet rs;
+		int num = -1;
+		try {
+			stmt = conn.prepareStatement("SELECT * FROM books WHERE isbn_num=?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			stmt.setInt(1, iSBN);
+			rs = stmt.executeQuery();
+			rs.beforeFirst();
+			while (rs.next())
+			num = rs.getInt(4);
+			System.out.println("Num copies available for " + iSBN + " equals " + num);
+		} catch (Exception e){
+			System.out.println(e + "239");
+		}
+		if (num >=1){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public static void removeCheckoutRecord(int iD, int iSBN){
+		PreparedStatement stmt;
+		try {
+			stmt = conn.prepareStatement("DELETE FROM books2students WHERE studentid=? AND bookissued=?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			stmt.setInt(1, iD);
+			stmt.setString(2, iSBN+"");
+			System.out.println("DELETE FROM books2students WHERE studentid= " + iD + " AND bookissued= " + iSBN);
+			stmt.executeUpdate();
+			System.out.println(iSBN + " removed from student " + iD);
+		} catch (Exception e){
+			System.out.println(e + "258");
+		}
+		incrimentNumCount(iSBN);
+	}
+
+	public static void createCheckoutRecord(int iD, int iSBN) {
+		PreparedStatement stmt;
+		//First block creates the relation
+		if (Available (iSBN)){
+			try {
+				stmt = conn.prepareStatement("INSERT INTO books2students (studentid, bookissued, issuedate) VALUES" + "(?, ?, ?)", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+				stmt.setInt(1, iD);
+				stmt.setInt(2, iSBN);
+				stmt.setInt(3, Calendar.DATE);
+				Calendar cal = new GregorianCalendar();
+				cal.add(Calendar.DATE, 30);
+				//stmt.setInt(4,  cal.getTime());
+				stmt.executeUpdate();
+				System.out.println("ADDED TO STU/BOOK");
+			} catch (Exception e){
+				System.out.println(e + "275");
+			}	
+		}
+		else {
+			System.out.println("Unable to offer that book for checkout.");
+		}
 	}
 }
--- src/fourThirtya2/ModelView.java
+++ src/fourThirtya2/ModelView.java
@@ -1,6 +1,7 @@
 package fourThirtya2;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class ModelView {
@@ -38,27 +39,37 @@ public class ModelView {
 			System.out.println("3. Remove a student from the database.");
 			System.out.println("4. Remove a book from the database.");
 			System.out.println("5. Issue a book to a student.");
-			System.out.println("6. Print the contents of a table.");
+			System.out.println("6. Return a book from a student.");
+			System.out.println("7. Print the contents of a table.");
 			System.out.println("0. Quit the program.");
 			kbd = new Scanner(System.in);
 			int select = kbd.nextInt();
 			switch (select) {
 			case 1: // EXECUTE ADD STUDENT METHOD
 				clearConsole();
+				addStudent();
 				break;
 			case 2: // EXECUTE ADD BOOK METHOD
 				clearConsole();
+				addBook();
 				break;
 			case 3: // EXECUTE REMOVE STUDENT METHOD
 				clearConsole();
+				removeStudent();
 				break;
 			case 4: // EXECUTE REMOVE BOOK METHOD
 				clearConsole();
+				removeBook();
 				break;
 			case 5: // EXECUTE ISSUE BOOK TO STUDENT METHOD
 				clearConsole();
+				issueBook();
 				break;
 			case 6: // EXECUTE TABLE PRINT METHOD
+				returnBook();
+				clearConsole();
+				break;
+			case 7: // EXECUTE TABLE PRINT METHOD
 				printTable();
 				clearConsole();
 				break;
@@ -76,24 +87,116 @@ public class ModelView {
 		kbd.close();
 	}
 	
+	private void returnBook() {
+		System.out.println("Please type the ID of the student.");
+		Scanner kbd = new Scanner(System.in);
+		int ID = -1;
+		try {
+			ID = Integer.parseInt(kbd.nextLine());
+		} catch (NumberFormatException e) {
+		    System.out.println(e);
+		}
+		System.out.println("Please type the ISBN of the book to return.");
+		int iSBN = -1;
+		try {
+			iSBN = Integer.parseInt(kbd.nextLine());
+		} catch (NumberFormatException e) {
+		    System.out.println(e);
+		}
+		Backend.removeCheckoutRecord(ID, iSBN);
+	}
+
 	public void addStudent(){
-		
+		System.out.println("Please type the ID of the student.");
+		Scanner kbd = new Scanner(System.in);
+		int ID = -1;
+		try {
+			ID = Integer.parseInt(kbd.nextLine());
+		} catch (NumberFormatException e) {
+		    System.out.println(e);
+		}
+		System.out.println("Please type the first name of the student.");
+		String fname = kbd.nextLine();
+		System.out.println("Please type the last name of the student.");
+		String lname = kbd.nextLine();
+		System.out.println("Please type the degree of the student.");
+		String degree = kbd.nextLine();
+		Backend.addStudent(ID, fname, lname, degree);
 	}
 	
 	public void addBook(){
-		
+		System.out.println("Please tpye the ISBN number of the book.");
+		Scanner kbd = new Scanner(System.in);
+		int ISBN = -1;
+		try {
+			ISBN = Integer.parseInt(kbd.nextLine());
+		} catch (NumberFormatException e) {
+		    System.out.println(e);
+		}
+		if (Backend.haveBook(ISBN)) {
+				// JUST NEED TO INCREMENT THE COUNTER OF AVAILABLE BOOKS
+				Backend.incrimentNumCount(ISBN);
+		}
+		else {
+			System.out.println("Please type the book's year of publication.");
+			int year = -1;
+			try {
+				year = Integer.parseInt(kbd.nextLine());
+			} catch (NumberFormatException e) {
+			    System.out.println(e);
+			}
+			System.out.println("Please type the name of the book.");
+			String name = kbd.nextLine();
+			Backend.addBook(ISBN, name, year);
+		}
 	}
 	
 	public boolean removeStudent(){
+		System.out.println("Please type the ID of the student.");
+		Scanner kbd = new Scanner(System.in);
+		int ID = -1;
+		try {
+			ID = Integer.parseInt(kbd.nextLine());
+		} catch (NumberFormatException e) {
+		    System.out.println(e);
+		}
+		if (Backend.removeStudent(ID)){
+			System.out.println("Removed student " + ID );
+		}
+		else {
+			System.out.println("Error removing student.");
+		}
 		return false;
 	}
 	
-	public boolean removeBook(){
-		return false;
+	public void removeBook(){
+		System.out.println("Please type the ISBN number of the book you'd like to remove.");
+		Scanner kbd = new Scanner(System.in);
+		int ID = kbd.nextInt();
+		Backend.removeBook(ID);
 	}
 	
 	public void issueBook(){
-		
+		Scanner kbd = new Scanner(System.in);
+		System.out.println("Please type the student ID.");
+		int ID = -1;
+		try {
+			ID = Integer.parseInt(kbd.nextLine());
+		} catch (NumberFormatException e) {
+		    System.out.println(e);
+		}
+		System.out.println("Please type the book ISBN number.");
+		int ISBN = -1;
+		try {
+			ISBN = Integer.parseInt(kbd.nextLine());
+		} catch (NumberFormatException e) {
+		    System.out.println(e);
+		}
+		if (Backend.Available(ISBN)){
+			System.out.println("Issuing " + ISBN + " to " + ID);
+			Backend.createCheckoutRecord(ID, ISBN);
+			Backend.decrimentBookCount(ISBN);
+		}
 	}
 	
 	/*
@@ -126,8 +229,7 @@ public class ModelView {
 				String tableName = tables[select];
 				rs = Backend.getTable(tableName);
 				while (rs.next()) {
-					System.out.println(rs.getString("test_id") + "\t,\t"
-							+ rs.getString("test_name"));
+					System.out.println(rs.getInt(1) + "\t,\t" + rs.getString(2)+ "\t,\t" + rs.getInt(3)+ "\t,\t" + rs.getString(4));
 				}
 			}
 		} catch (Exception e) {
